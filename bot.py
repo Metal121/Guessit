61d21cb4c83cd84ae2b106e6bd43699c7d14eae2
Metal121 <37195556+Metal121@users.noreply.github.com>
2019-04-07 10:01:16 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,46 +6,53 @@ import random
 import requests
 import os
 
-print("Connecting...")
-
-client = commands.Bot(command_prefix = "!")
+client = commands.Bot(command_prefix="!")
+player_dict = dict()
 
 
 @client.event
 async def on_ready():
-	print("Logged in as:\n{}/{}#{}\n----------".format(client.user.id, client.user.name, client.user.discriminator))
-	await client.change_presence(activity=discord.Game(name="with metal currently"))
+    print("Bot ist bereit")
+    
 
-@client.command()
-async def startgtn(ctx, message: discord.Message = None):
-	embed=discord.Embed(color=0x4A90E2)
-	embed.add_field(name="I'm thinking of a new number:", value="Guess the number and i'll tell you if it's higher or lower then your guess!", inline=False)
-	embed.set_footer(text="It's between 1 and 1000  | Currently in beta | Developed by Metal#0110")
-	await ctx.send(embed=embed)
-	number = random.randint(1,1000)
-	while True:
-		guess = await client.wait_for("message", check=lambda message: message.author == ctx.author)
-		if guess.content.lstrip("-").lstrip(".").isdigit() == True:
-			if int(guess.content) > number:
-				embed=discord.Embed(color=0x00ffff)
-				embed.add_field(name="Lower:", value="It's lower than **{}**".format(guess.content), inline=False)
-				await ctx.send(embed=embed)
-			
-			if int(guess.content) < number:
-				embed=discord.Embed(color=0x00ffff)
-				embed.add_field(name="Higher:", value="It's higher than **{}**".format(guess.content), inline=False)
-				await ctx.send(embed=embed)
-			
-			if int(guess.content) == number:
-				await ctx.send(ctx.author.mention)
-				embed=discord.Embed(color=0x00ff40)
-				embed.add_field(name="{} Guessed correctly.".format(ctx.author), value="I was thinking of the number {}".format(number), inline=False)
-				await ctx.send(embed=embed)
-				break
-		else:	
-			if guess.content == "quit" or guess.content == "exit" and ctx.author == guess.author:
-				break
-			else:
-				continue
 
-client.run(str(os.environ.get("BOT_TOKEN")))
+@client.command(pass_context=True)
+async def play (ctx, url):
+    channel = ctx.message.author.voice_channel
+    await client.join_voice_channel(channel)
+    server = ctx.message.server
+    voice = client.voice_client_in(server)
+    player = await voice.create_ytdl_player(url)
+    player_dict[server.id] = player
+    await client.send_message(ctx.message.channel, "Playing `%s` now" % player.title)
+    player.start()
+
+
+@client.command(pass_context=True)
+async def stop(ctx):
+    server = ctx.message.server
+    player = player_dict[server.id]
+    player.stop()
+    await client.send_message(ctx.message.channel, "Stopped `%s`" % player.title)
+    del player_dict[server.id]
+
+
+@client.command(pass_context=True)
+async def pause(ctx):
+    server = ctx.message.server
+    player = player_dict[server.id]
+    player.pause()
+    await client.send_message(ctx.message.channel, "Paused `%s`" % player.title)
+
+
+@client.command(pass_context=True)
+async def resume(ctx):
+    server = ctx.message.server
+    player = player_dict[server.id]
+    player.resume()
+    await client.send_message(ctx.message.channel, "Resumed `%s`" % player.title)
+
+
+
+
+client.run(str(os.environ.get('BOT_TOKEN')))
